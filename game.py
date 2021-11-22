e27a3d76f7a53ae1216520fa99c8dcc99698f420
EricHayter <eric.hayter@ocsbstudent.ca>
2021-11-22 17:42:44 -0500

    added basic typing functionality

--- game.py
+++ game.py
@@ -1,23 +1,40 @@
 import curses
 
-stdscr = curses.initscr()
-stdscr.keypad(0)
-curses.noecho()
+def game(stdscr):
+  stdscr.keypad(0)
+  curses.noecho()
+  
+  string = "you should not be able to change this"
 
-string = "This is a test"
+  stdscr.addstr(string)
+  stdscr.move(0,0)
 
-stdscr.addstr(string)
+  while True:
+    input = stdscr.getch()
 
-stdscr.move(0, 0)
+    # if the user pressed escape in the program it will close
+    if input == 27:
+      break
 
-while True:
-  input = stdscr.getch()
+    elif input == 8:
+      try:
+        pos = stdscr.getyx()
+        stdscr.addstr(pos[0],pos[1],string[pos[1]])
+        stdscr.move(pos[0],pos[1]-1)
+      except:
+        stdscr.move(0,0)
+        pass
 
-  # if the user pressed escape in the program it will close
-  if input == 27:
-    break
-
-  stdscr.addstr(chr(input))
+    else: 
+      stdscr.addstr(chr(input))
 
 
-curses.endwin()
+  curses.endwin()
+
+def main():
+    curses.wrapper(game)
+
+if (__name__ == "__main__"):
+    main()
+
+main()
