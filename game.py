316f8aec279137380d5bcd0df564b5107a208ab1
EricHayter <eric.hayter@ocsbstudent.ca>
2021-11-22 17:53:27 -0500

    Finished Tutorial on simple typing game
    need to implement menu for multiplayer and look into hosting on local network

--- game.py
+++ game.py
@@ -1,40 +1,79 @@
 import curses
-
-def game(stdscr):
-  stdscr.keypad(0)
-  curses.noecho()
-  
-  string = "you should not be able to change this"
-
-  stdscr.addstr(string)
-  stdscr.move(0,0)
-
-  while True:
-    input = stdscr.getch()
-
-    # if the user pressed escape in the program it will close
-    if input == 27:
-      break
-
-    elif input == 8:
-      try:
-        pos = stdscr.getyx()
-        stdscr.addstr(pos[0],pos[1],string[pos[1]])
-        stdscr.move(pos[0],pos[1]-1)
-      except:
-        stdscr.move(0,0)
-        pass
-
-    else: 
-      stdscr.addstr(chr(input))
+from curses import wrapper
+import time
+import random
 
 
-  curses.endwin()
+def start_screen(stdscr):
+	stdscr.clear()
+	stdscr.addstr("Welcome to Type-Mania!")
+	stdscr.addstr("\nPress key to start!")
+	stdscr.refresh()
+	stdscr.getkey()
 
-def main():
-    curses.wrapper(game)
+def display_text(stdscr, target, current, wpm=0):
+	stdscr.addstr(target)
+	stdscr.addstr(1, 0, f"WPM: {wpm}")
 
-if (__name__ == "__main__"):
-    main()
+	for i, char in enumerate(current):
+		correct_char = target[i]
+		color = curses.color_pair(1)
+		if char != correct_char:
+			color = curses.color_pair(2)
 
-main()
+		stdscr.addstr(0, i, char, color)
+
+def load_text():
+	with open("text.txt", "r") as f:
+		lines = f.readlines()
+		return random.choice(lines).strip()
+
+def wpm_test(stdscr):
+	target_text = load_text()
+	current_text = []
+	wpm = 0
+	start_time = time.time()
+	stdscr.nodelay(True)
+
+	while True:
+		time_elapsed = max(time.time() - start_time, 1)
+		wpm = round((len(current_text) / (time_elapsed / 60)) / 5)
+
+		stdscr.clear()
+		display_text(stdscr, target_text, current_text, wpm)
+		stdscr.refresh()
+
+		if "".join(current_text) == target_text:
+			stdscr.nodelay(False)
+			break
+
+		try:
+			key = stdscr.getkey()
+		except:
+			continue
+
+		if ord(key) == 27:
+			break
+
+		if key in ("KEY_BACKSPACE", '\b', "\x7f"):
+			if len(current_text) > 0:
+				current_text.pop()
+		elif len(current_text) < len(target_text):
+			current_text.append(key)
+
+
+def main(stdscr):
+	curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+	curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+	curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+
+	start_screen(stdscr)
+	while True:
+		wpm_test(stdscr)
+		stdscr.addstr(2, 0, "You completed the text! Press any key to continue...")
+		key = stdscr.getkey()
+		
+		if ord(key) == 27:
+			break
+
+wrapper(main)
