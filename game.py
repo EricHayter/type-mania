400d1ba3e17eb84a3bb4662039d9640f3342ceb8
EricHayter <eric.hayter@ocsbstudent.ca>
2021-11-23 17:21:21 -0500

    added menu selection for the user

--- game.py
+++ game.py
@@ -3,77 +3,123 @@ from curses import wrapper
 import time
 import random
 
+menu = [" TYPE-MANIA ", " Singleplayer ", " Multiplayer ", " Exit "]
+
+
+def print_menu(stdscr, selected_row_idx):
+    stdscr.clear()
+    h, w = stdscr.getmaxyx()
+
+    for idx, row in enumerate(menu):
+        x = w//2 - len(row)//2
+        y = h//2 - len(menu) + idx
+        if idx == selected_row_idx:
+            stdscr.attron(curses.color_pair(4))
+            stdscr.addstr(y, x, row)
+            stdscr.attroff(curses.color_pair(4))
+        else:
+            stdscr.addstr(y, x, row)
+
+    stdscr.refresh()
+
 
 def start_screen(stdscr):
-	stdscr.clear()
-	stdscr.addstr("Welcome to Type-Mania!")
-	stdscr.addstr("\nPress key to start!")
-	stdscr.refresh()
-	stdscr.getkey()
+
+    current_row_idx = 1
+
+    print_menu(stdscr, current_row_idx)
+
+    while 1:
+        key = stdscr.getch()
+
+        stdscr.clear()
+
+        if key in [curses.KEY_UP, 450] and current_row_idx > 1:
+            current_row_idx -= 1
+
+        elif key in [curses.KEY_DOWN, 456] and current_row_idx < 3:
+            current_row_idx += 1
+
+        elif key in [curses.KEY_ENTER, 10, 13]:
+            stdscr.clear()
+            stdscr.addstr(0, 0, f"You  pressed{menu[current_row_idx]}")
+            stdscr.refresh()
+            stdscr.getch()
+
+        print_menu(stdscr, current_row_idx)
+        stdscr.refresh()
+    # stdscr.getkey()
+
 
 def display_text(stdscr, target, current, wpm=0):
-	stdscr.addstr(target)
-	stdscr.addstr(1, 0, f"WPM: {wpm}")
+    stdscr.addstr(target)
+    stdscr.addstr(1, 0, f"WPM: {wpm}")
 
-	for i, char in enumerate(current):
-		correct_char = target[i]
-		color = curses.color_pair(1)
-		if char != correct_char:
-			color = curses.color_pair(2)
+    for i, char in enumerate(current):
+        correct_char = target[i]
+        color = curses.color_pair(1)
+        if char != correct_char:
+            color = curses.color_pair(2)
 
-		stdscr.addstr(0, i, char, color)
+        stdscr.addstr(0, i, char, color)
+
 
 def load_text():
-	with open("text.txt", "r") as f:
-		lines = f.readlines()
-		return random.choice(lines).strip()
+    with open("text.txt", "r") as f:
+        lines = f.readlines()
+        return random.choice(lines).strip()
+
 
 def wpm_test(stdscr):
-	target_text = load_text()
-	current_text = []
-	wpm = 0
-	start_time = time.time()
-	stdscr.nodelay(True)
+    target_text = load_text()
+    current_text = []
+    wpm = 0
+    start_time = time.time()
+    stdscr.nodelay(True)
 
-	while True:
-		time_elapsed = max(time.time() - start_time, 1)
-		wpm = round((len(current_text) / (time_elapsed / 60)) / 5)
+    while True:
+        time_elapsed = max(time.time() - start_time, 1)
+        wpm = round((len(current_text) / (time_elapsed / 60)) / 5)
 
-		stdscr.clear()
-		display_text(stdscr, target_text, current_text, wpm)
-		stdscr.refresh()
+        stdscr.clear()
+        display_text(stdscr, target_text, current_text, wpm)
+        stdscr.refresh()
 
-		if "".join(current_text) == target_text:
-			stdscr.nodelay(False)
-			break
+        if "".join(current_text) == target_text:
+            stdscr.nodelay(False)
+            break
 
-		try:
-			key = stdscr.getkey()
-		except:
-			continue
+        try:
+            key = stdscr.getkey()
+        except:
+            continue
 
-		if ord(key) == 27:
-			break
+        if ord(key) == 27:
+            break
 
-		if key in ("KEY_BACKSPACE", '\b', "\x7f"):
-			if len(current_text) > 0:
-				current_text.pop()
-		elif len(current_text) < len(target_text):
-			current_text.append(key)
+        if key in ("KEY_BACKSPACE", '\b', "\x7f"):
+            if len(current_text) > 0:
+                current_text.pop()
+        elif len(current_text) < len(target_text):
+            current_text.append(key)
 
 
 def main(stdscr):
-	curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
-	curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-	curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.curs_set(0)
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(4, curses.COLOR_BLACK, curses.COLOR_WHITE)
 
-	start_screen(stdscr)
-	while True:
-		wpm_test(stdscr)
-		stdscr.addstr(2, 0, "You completed the text! Press any key to continue...")
-		key = stdscr.getkey()
-		
-		if ord(key) == 27:
-			break
+    start_screen(stdscr)
+    while True:
+        wpm_test(stdscr)
+        stdscr.addstr(
+            2, 0, "You completed the text! Press any key to continue...")
+        key = stdscr.getkey()
+
+        if ord(key) == 27:
+            break
+
 
 wrapper(main)
