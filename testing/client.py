386072f010d8fb4af07f57ddf85cae4f121653b7
Eric <hayter.eric@gmail.com>
2022-02-28 09:04:52 -0500

    working on overhualing the client

--- testing/client.py
+++ testing/client.py
@@ -1,18 +1,40 @@
 import socket
-import random
-import time
-import json
 
-HOST = '127.0.0.1'
-PORT = 4321
 
-score = {}
-scores = {}
+class Client():
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('127.0.0.1', 4321))
+    def __init__(self):
+        self.HEADER = 64
+        self.PORT = 5050
+        self.FORMAT = 'utf-8'
+        self.DISCONNECT_MESSAGE = "!DISCONNECT"
+        self.SERVER = '127.0.0.1'
+        self.ADDR = (self.SERVER, self.PORT)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-print(f'connected to {HOST} on port {PORT}')
+    def connect(self):
+        self.sock.connect(self.ADDR)
 
-for x in range(1, 10):
-    print(repr(s.recv(1024)))
+    def send(self, msg):
+        message = msg.encode(self.FORMAT)
+        msg_length = len(message)
+        send_length = str(msg_length).encode(self.FORMAT)
+        send_length += b' ' * (self.HEADER - len(send_length))
+        self.sock.send(send_length)
+        self.sock.send(message)
+        print(self.sock.recv(2048).decode(self.FORMAT))
+
+    def disconnect(self):
+        self.send(self.DISCONNECT_MESSAGE)
+
+
+if __name__ == "__main__":
+    c = Client()
+    c.connect()
+
+    c.send("Hello World!")
+    input()
+    c.send("Hello Everyone!")
+    input()
+    c.send("Hello Eric!")
+    c.disconnect()
