e41d673c18cc0f3f660ef0939fad76228569a269
Eric <hayter.eric@gmail.com>
2022-02-28 09:05:03 -0500

    overhauling the server

--- testing/server.py
+++ testing/server.py
@@ -1,86 +1,51 @@
-import types
 import socket
-import selectors
-import time
-import json
-
-player_scores = {}
-sel = selectors.DefaultSelector()
+from telnetlib import SE
+import threading
 
 
-def accept_wrapper(sock):
-    conn, addr = sock.accept()
-    print('accepted connection from ', addr)
-    conn.setblocking(False)
-    data = types.SimpleNamespace(addr=addr, inb=b'', outb=b'')
-    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    sel.register(conn, events, data=data)
+class Server():
 
+    def __init__(self):
+        self.HEADER = 64
+        self.PORT = 5050
+        self.SERVER = '127.0.0.1'
+        self.ADDR = (self.SERVER, self.PORT)
+        self.FORMAT = 'utf-8'
+        self.DISCONNECT_MESSAGE = "!DISCONNECT"
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-def service_connection(key, mask):
-    sock = key.fileobj
-    data = key.data
-    if mask & selectors.EVENT_READ:
-        recv_data = sock.recv(1024)
-        if recv_data:
-            player_scores.update(json.loads(recv_data))
-            data.outb += bytes(json.dumps(player_scores), 'utf-8')
+    def bind(self):
+        self.server.bind(self.ADDR)
 
-        else:
-            print('closing connection to ', data.addr)
-            sel.unregister(sock)
-            sock.close()
-    if mask & selectors.EVENT_WRITE:
-        if data.outb:
-            sent = sock.send(data.outb)
-            data.outb = data.outb[sent:]
+    def handle_client(self, conn, addr):
+        print(f"[NEW CONNECTION] {addr} connected.")
 
+        connected = True
+        while connected:
+            msg_length = conn.recv(self.HEADER).decode(self.FORMAT)
+            if msg_length:
+                msg_length = int(msg_length)
+                msg = conn.recv(msg_length).decode(self.FORMAT)
+                if msg == self.DISCONNECT_MESSAGE:
+                    connected = False
 
-def client(getScoresFunction, setScoresFunction, sock):
-    while True:
-        # sending local player's score
-        sock.send(bytes(json.dumps(getScoresFunction()), 'utf-8'))
-        server_response = json.loads(sock.recv(1024))
-        # server responds with everyone's score in JSON format
-        try:
-            if closeClient(server_response):
-                return
-            setScoresFunction(server_response)
-        except:
-            pass
-        time.sleep(0.1)
+                print(f"[{addr}] {msg}")
+                conn.send("Msg received".encode(self.FORMAT))
 
+        conn.close()
 
-def closeClient(scores):
-    for i in scores.keys():
-        if scores[i] != 100:
-            return False
-    return True
-
-
-def server():
-    try:
-        HOST = '127.0.0.1'
-        PORT = 4321
-
-        lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        lsock.bind((HOST, PORT))
-        lsock.listen()
-        print('listening on ', (HOST, PORT))
-        lsock.setblocking(False)
-        sel.register(lsock, selectors.EVENT_READ, data=None)
-
+    def start(self):
+        self.server.listen()
+        print(f"[LISTENING] Server is listening on {self.SERVER}")
         while True:
-            events = sel.select(timeout=None)
-            for key, mask in events:
-                if key.data is None:
-                    accept_wrapper(key.fileobj)
-                else:
-                    service_connection(key, mask)
-    except Exception as e:
-        print(e)
-        lsock.close()
+            conn, addr = self.server.accept()
+            thread = threading.Thread(target=self.handle_client,
+                                      args=(conn, addr))
+            thread.start()
+            print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 6}")
 
 
 if __name__ == "__main__":
-    server()
+    print("[STARTING] server is starting...")
+    s = Server()
+    s.start()
