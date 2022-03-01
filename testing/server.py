6f823ff40eeb77082b5fccc012abc42b0edd7131
Eric <hayter.eric@gmail.com>
2022-02-28 21:14:31 -0500

    server work done

--- testing/server.py
+++ testing/server.py
@@ -1,42 +1,64 @@
+import json
 import socket
-from telnetlib import SE
 import threading
+import time
 
 
 class Server():
+    # class variablees
+    HEADER = 64
+    SERVER = '127.0.0.1'
+    DISCONNECT_MESSAGE = "!DISCONNECT"
+    FORMAT = 'utf-8'
 
+    # instance variables
     def __init__(self):
-        self.HEADER = 64
         self.PORT = 5050
-        self.SERVER = '127.0.0.1'
-        self.ADDR = (self.SERVER, self.PORT)
-        self.FORMAT = 'utf-8'
-        self.DISCONNECT_MESSAGE = "!DISCONNECT"
+        self.ADDR = (Server.SERVER, self.PORT)
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.scores = {}
 
     def bind(self):
         self.server.bind(self.ADDR)
 
     def handle_client(self, conn, addr):
         print(f"[NEW CONNECTION] {addr} connected.")
+        msg_length = conn.recv(Server.HEADER).decode(Server.FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(self.FORMAT)
+            print(repr(msg))
+            try:
+                self.scores.update(json.loads(msg))
+                print("THIS IS WORKING")
+            except:
+                print(f"[{addr}] Disconnected")
+                return
 
-        connected = True
-        while connected:
-            msg_length = conn.recv(self.HEADER).decode(self.FORMAT)
+        while len(self.scores) < 1:
+            time.sleep(0.5)
+            print(len(self.scores))
+
+        conn.send(json.dumps(self.scores).encode(self.FORMAT))
+        print("this is being sent")
+
+        while True:
+            msg_length = conn.recv(Server.HEADER).decode(Server.FORMAT)
             if msg_length:
                 msg_length = int(msg_length)
                 msg = conn.recv(msg_length).decode(self.FORMAT)
                 if msg == self.DISCONNECT_MESSAGE:
-                    connected = False
+                    print(f"[{addr}] Disconnected")
+                    break
 
-                print(f"[{addr}] {msg}")
+                print(repr(msg))
                 conn.send("Msg received".encode(self.FORMAT))
 
         conn.close()
 
     def start(self):
         self.server.listen()
-        print(f"[LISTENING] Server is listening on {self.SERVER}")
+        print(f"[LISTENING] Server is listening on {Server.SERVER}")
         while True:
             conn, addr = self.server.accept()
             thread = threading.Thread(target=self.handle_client,
@@ -48,4 +70,5 @@ class Server():
 if __name__ == "__main__":
     print("[STARTING] server is starting...")
     s = Server()
+    s.bind()
     s.start()
